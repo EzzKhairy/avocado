50ca2f099e74fb1d03f0025dc286b1343e4bb5c1
omarsherif15 <omarsherifmetwaly@gmail.com>
2022-04-27 22:29:45 +0200

    View & Edit Profile Api Connected

---
+++ lib/models/login_model.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+class LoginModel
+{
+  String? status;
+  String? message;
+  String? accessToken;
+  Lawyer? lawyersData;
+
+  LoginModel.fromJson(Map<String, dynamic> json){
+    status = json['status'];
+    message = json['message'];
+    accessToken = json['access_token'];
+    lawyersData = json['data'] != null ? Lawyer.fromJson(json['data']) : null;
+  }
+}
+
+class Lawyer
+{
+  int? id;
+  String? status;
+  String? lawyerNationalNumber;
+  String? name;
+  String? email;
+  String? profilePhoto;
+  String? address;
+  String? role;
+  String? createdAt;
+  String? updatedAt;
+
+  Lawyer.fromJson(Map<String,dynamic> json)
+  {
+    id = json['id'];
+    status = json['status'];
+    lawyerNationalNumber = json['Lawyer_National_Number'];
+    name = json['name'];
+    email = json['email'];
+    profilePhoto = json['profile_photo_path'];
+    address = json['address'];
+    role = json['Role'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+  }
+}
+
